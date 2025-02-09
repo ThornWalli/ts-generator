dbe4cbab4120e6b90e5c911168b59f0240777d9e
Thorn Walli <lammpee@gmail.com>
2025-02-09 01:40:17 +0100

    test(fixtures): update index.ts

--- test/fixtures/index.ts
+++ test/fixtures/index.ts
@@ -1,7 +1,12 @@
-import { Observable } from 'rxjs';
 import { operatorA, operatorB, operatorC } from './operators';
-export function test<T>() {
+import { Observable } from 'rxjs';
+export function foo<T>() {
   return (source: Observable<T>): Observable<T> => {
     return source.pipe(operatorA(), operatorB(), operatorC());
+  };
+}
+export function bar<T>() {
+  return (source: Observable<T>): Observable<T> => {
+    return source.pipe(operatorA());
   };
 }
