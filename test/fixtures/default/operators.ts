2f7db7799e94466914fc8a34b230153e797d7b42
Thorn Walli <lammpee@gmail.com>
2025-02-09 14:49:50 +0100

    test(default): fix missing definitions

--- test/fixtures/default/operators.ts
+++ test/fixtures/default/operators.ts
@@ -1,13 +1,13 @@
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Dummy } from './types';
+import { empty } from '../../utils';
 
-const empty = source => source;
 export function operatorA() {
   return empty;
 }
-export function operatorB(text: string, numeric: number) {
-  return source => source.pipe(tap(() => console.log({ text, numeric })));
+export function operatorB<T>(text: string, numeric: number) {
+  return (source:Observable<T>) => source.pipe(tap(() => console.log({ text, numeric })));
 }
-export function operatorDummy(dummy: Dummy) {
-  return source => source.pipe(tap(() => console.log(dummy)));
+export function operatorDummy<T>(dummy: Dummy) {
+  return (source: Observable<T>) => source.pipe(tap(() => console.log(dummy)));
 }
--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,6 @@
 import { readFile } from 'fs/promises';
 import { basename } from 'path';
+import { Observable } from 'rxjs';
 
 export function getFixtureConfig(fixture: string) {
   const filepath = `test/fixtures/${fixture}/config.json`;
@@ -10,4 +11,8 @@ export async function getFixtureFile(fixture: string) {
   const filepath = `test/fixtures/${fixture}/index.ts`;
   const content = await readFile(filepath, 'utf-8');
   return { filename: basename(filepath), content };
+}
+
+export function empty<T>(source: Observable<T>): Observable<T> {
+  return source;
 }
